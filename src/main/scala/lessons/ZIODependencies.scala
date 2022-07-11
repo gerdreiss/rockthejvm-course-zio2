2dd70ace5f4542c4e83b782b5a8dc96f9d188a0d
Gerd Reiss <gerdreiss@@users.noreply.github.com>
2022-07-11 15:44:27 +0200

    [Rock the JVM Course ZIO 2] Dependencies and ZLayers, Part 2

--- src/main/scala/lessons/ZIODependencies.scala
+++ src/main/scala/lessons/ZIODependencies.scala
@@ -2,52 +2,10 @@ package lessons
 
 import zio.*
 
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+
 object ZIODependencies extends ZIOAppDefault:
-
-  // app to subscribe users to newsletter
-  case class User(name: String, email: String)
-
-  case class Connection():
-    def runQuery(query: String): Task[Unit] =
-      Console.printLine(s"Running query: $query")
-
-  class UserSubscription(emailService: EmailService, userDatabase: UserDatabase):
-    def subscribeUser(user: User): Task[Unit] =
-      emailService.sendEmail(user) *>
-        userDatabase.insertUser(user)
-
-  object UserSubscription:
-    def make(emailService: EmailService, userDatabase: UserDatabase): UserSubscription =
-      new UserSubscription(emailService, userDatabase)
-
-  class EmailService():
-    def sendEmail(user: User): Task[Unit] =
-      Console.printLine(s"Subscribed user ${user.name} to newsletter")
-
-  object EmailService:
-    def make: EmailService =
-      new EmailService()
-
-  class UserDatabase(connectionPool: ConnectionPool):
-    def insertUser(user: User): Task[Unit] =
-      connectionPool.getConnection
-        .flatMap {
-          _.runQuery(s"INSERT INTO users (name, email) VALUES ('${user.name}', '${user.email}')")
-        }
-
-  object UserDatabase:
-    def make(connectionPool: ConnectionPool): UserDatabase =
-      new UserDatabase(connectionPool)
-
-  class ConnectionPool(numConns: Int):
-    def getConnection: Task[Connection] =
-      Console
-        .printLine(s"Connected to database.")
-        .as(Connection())
-
-  object ConnectionPool:
-    def make(numConns: Int): ConnectionPool =
-      new ConnectionPool(numConns)
 
   val subscriptionService: UIO[UserSubscription] =
     ZIO.succeed(
@@ -64,20 +22,65 @@ object ZIODependencies extends ZIOAppDefault:
 
   // alternative
   def subscribe2(user: User): ZIO[UserSubscription, Throwable, Unit] =
-    for
-      subscription <- ZIO.service[UserSubscription]
-      _            <- subscription.subscribeUser(user)
-    yield ()
+    // for
+    //   subscription <- ZIO.service[UserSubscription]
+    //   _            <- subscription.subscribeUser(user)
+    // yield ()
+    ZIO.serviceWithZIO[UserSubscription](_.subscribeUser(user))
 
-  override def run: ZIO[Any, Any, Any] =
+  /**
+   * Layers
+   */
+  val connectionPoolLayer: ULayer[ConnectionPool] =
+    ZLayer.succeed(ConnectionPool.make(10))
+
+  val databaseLayer: URLayer[ConnectionPool, UserDatabase] =
+    ZLayer.fromFunction(UserDatabase.make)
+
+  val databaseLayerFull: ULayer[UserDatabase] =
+    connectionPoolLayer >>> databaseLayer
+
+  val emailServiceLayer: ULayer[EmailService] =
+    ZLayer.succeed(EmailService.make)
+
+  val subscriptionRequirementLayer: ULayer[UserDatabase & EmailService] =
+    databaseLayerFull ++ emailServiceLayer
+
+  val userSubscriptionLayer: URLayer[EmailService & UserDatabase, UserSubscription] =
+    ZLayer.fromFunction(UserSubscription.make)
+
+  val userSubscriptionLayerFull: ULayer[UserSubscription] =
+    subscriptionRequirementLayer >>> userSubscriptionLayer
+
+  val userSubscriptionLayerFull2: ULayer[UserSubscription] =
+    ZLayer.make[UserSubscription](
+      ConnectionPool.live(10),
+      UserDatabase.live.fresh, // new instance for each layer
+      EmailService.live,
+      UserSubscription.live
+    )
+
+  val dbWithPoolLayer: URLayer[ConnectionPool, ConnectionPool & UserDatabase] =
+    UserDatabase.live.passthrough
+
+  val dbService: URLayer[UserDatabase, UserDatabase] =
+    ZLayer.service[UserDatabase]
+
+  val subscriptionLaunch: URIO[EmailService & UserDatabase, Nothing] =
+    UserSubscription.live.launch
+
+  /**
+   * Standard services:
+   *  - Clock
+   *  - Random
+   *  - System
+   *  - Console
+   */
+  val getTime: UIO[Long]                                = Clock.currentTime(TimeUnit.SECONDS)
+  val randomValue: UIO[Int]                             = Random.nextInt
+  val systemInfo: IO[SecurityException, Option[String]] = System.env("os.name")
+  val printEffect: IO[IOException, Unit]                = Console.printLine("This is ZIO")
+
+  override def run: Task[Unit] =
     subscribe2(User("John", "john@mail.com"))
-      .provideLayer(
-        ZLayer.succeed(
-          UserSubscription.make(
-            EmailService.make,
-            UserDatabase.make(
-              ConnectionPool.make(10)
-            )
-          )
-        )
-      )
+      .provide(userSubscriptionLayerFull2)
---
+++ src/main/scala/lessons/ZIODependenciesDomain.scala
@@ -0,0 +1,60 @@
+package lessons
+
+import zio.*
+
+// app to subscribe users to newsletter
+case class User(name: String, email: String)
+
+case class Connection():
+  def runQuery(query: String): Task[Unit] =
+    Console.printLine(s"Running query: $query")
+
+class EmailService():
+  def sendEmail(user: User): Task[Unit] =
+    Console.printLine(s"Subscribed user ${user.name} to newsletter")
+
+object EmailService:
+  def make: EmailService =
+    new EmailService()
+
+  val live: ULayer[EmailService] =
+    ZLayer.succeed(make)
+
+class ConnectionPool(numConns: Int):
+  def getConnection: Task[Connection] =
+    Console
+      .printLine(s"Connected to database.")
+      .as(Connection())
+
+object ConnectionPool:
+  def make(numConns: Int): ConnectionPool =
+    new ConnectionPool(numConns)
+
+  def live(numConns: Int): ULayer[ConnectionPool] =
+    ZLayer.succeed(make(numConns))
+
+class UserDatabase(connectionPool: ConnectionPool):
+  def insertUser(user: User): Task[Unit] =
+    connectionPool.getConnection
+      .flatMap {
+        _.runQuery(s"INSERT INTO users (name, email) VALUES ('${user.name}', '${user.email}')")
+      }
+
+object UserDatabase:
+  def make(connectionPool: ConnectionPool): UserDatabase =
+    new UserDatabase(connectionPool)
+
+  val live: URLayer[ConnectionPool, UserDatabase] =
+    ZLayer.fromFunction(make)
+
+class UserSubscription(emailService: EmailService, userDatabase: UserDatabase):
+  def subscribeUser(user: User): Task[Unit] =
+    emailService.sendEmail(user) *>
+      userDatabase.insertUser(user)
+
+object UserSubscription:
+  def make(emailService: EmailService, userDatabase: UserDatabase): UserSubscription =
+    new UserSubscription(emailService, userDatabase)
+
+  val live: URLayer[EmailService & UserDatabase, UserSubscription] =
+    ZLayer.fromFunction(make)
